59cebb7ae61498f07066f35d33071726ee7c14f6
smc2315 <choeyongtae085@gmail.com>
2023-12-28 17:19:05 +0900

    Initial Commit

---
+++ src/main/java/com/smc2315/blogsearch/BlogsearchApplication.java
@@ -0,0 +1,13 @@
+package com.smc2315.blogsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogsearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smc2315/blogsearch/BlogsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smc2315.blogsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
