740e561794b8abdbaee2a1ef1dbe7310cab261b9
smc2315 <choeyongtae085@gmail.com>
2024-01-03 18:09:30 +0900

    feat: PopularSearchService 추가

---
+++ src/main/java/com/smc2315/blogsearch/service/PopularSearchService.java
@@ -0,0 +1,25 @@
+package com.smc2315.blogsearch.service;
+
+import com.smc2315.blogsearch.dto.PopularSearchMapper;
+import com.smc2315.blogsearch.dto.response.PopularSearchResponse;
+import com.smc2315.blogsearch.repository.PopularSearchRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class PopularSearchService {
+
+    private final PopularSearchRepository popularSearchRepository;
+
+    public List<PopularSearchResponse> findTop10Keywords() {
+        return popularSearchRepository.findTop10Keywords().stream()
+                .map(PopularSearchMapper.INSTANCE::toPopularSearchResponse)
+                .collect(Collectors.toList());
+    }
+}
