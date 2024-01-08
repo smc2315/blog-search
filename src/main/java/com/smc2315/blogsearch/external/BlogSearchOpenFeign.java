2e7435e9c1bc147144d6b024a6ac1e1e99d82061
smc2315 <choeyongtae085@gmail.com>
2024-01-08 15:22:04 +0900

    feat: OpenFeign interface 추가

---
+++ src/main/java/com/smc2315/blogsearch/external/BlogSearchOpenFeign.java
@@ -0,0 +1,10 @@
+package com.smc2315.blogsearch.external;
+
+import com.smc2315.blogsearch.dto.response.ApiResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public interface BlogSearchOpenFeign {
+
+    @GetMapping
+    ApiResponse searchBlogs(String query, int page, String sort);
+}
