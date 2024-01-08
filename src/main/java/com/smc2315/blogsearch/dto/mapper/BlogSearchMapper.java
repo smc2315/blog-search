38b66fe0e022c37a75125d611d6d95e7ca232299
smc2315 <choeyongtae085@gmail.com>
2024-01-08 15:23:00 +0900

    feat: BlogSearchMapper 추가

---
+++ src/main/java/com/smc2315/blogsearch/dto/mapper/BlogSearchMapper.java
@@ -0,0 +1,14 @@
+package com.smc2315.blogsearch.dto.mapper;
+
+import com.smc2315.blogsearch.dto.response.ApiResponse;
+import com.smc2315.blogsearch.dto.response.BlogSearchResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BlogSearchMapper {
+
+    BlogSearchMapper INSTANCE = Mappers.getMapper(BlogSearchMapper.class);
+
+    BlogSearchResponse toBlogSearchResponse(ApiResponse apiResponse);
+}
