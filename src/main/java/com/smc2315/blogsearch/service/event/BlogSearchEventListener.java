cbaeee79e77d4f7bced8c98eb21ff72724e69e70
smc2315 <choeyongtae085@gmail.com>
2024-01-10 15:28:32 +0900

    feat: BlogSearchEventListener 추가

---
+++ src/main/java/com/smc2315/blogsearch/service/event/BlogSearchEventListener.java
@@ -0,0 +1,23 @@
+package com.smc2315.blogsearch.service.event;
+
+import com.smc2315.blogsearch.aop.BlogSearchEvent;
+import com.smc2315.blogsearch.dto.SendableParameter;
+import com.smc2315.blogsearch.service.PopularSearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class BlogSearchEventListener {
+
+    private final PopularSearchService popularSearchService;
+
+    @Async
+    @EventListener(classes = BlogSearchEvent.class)
+    public void handle(BlogSearchEvent event) {
+        SendableParameter params = event.getValue();
+        popularSearchService.increaseSearchCount(params.getKeyword());
+    }
+}
