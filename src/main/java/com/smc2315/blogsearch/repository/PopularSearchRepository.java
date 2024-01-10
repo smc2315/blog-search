0c473622649bccbcb2b28228d0ed23e9dd4daf60
smc2315 <choeyongtae085@gmail.com>
2024-01-10 15:32:45 +0900

    feat: PopularSearchRepository findByKeyword 추가

--- src/main/java/com/smc2315/blogsearch/repository/PopularSearchRepository.java
+++ src/main/java/com/smc2315/blogsearch/repository/PopularSearchRepository.java
@@ -3,5 +3,9 @@ package com.smc2315.blogsearch.repository;
 import com.smc2315.blogsearch.entity.PopularSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PopularSearchRepository extends JpaRepository<PopularSearch, Long>, CustomPopularSearchRepository {
+
+    Optional<PopularSearch> findByKeyword(String keyword);
 }
