13f1aa7c2c5853debe385b2b1992cfb958940e08
smc2315 <choeyongtae085@gmail.com>
2024-01-10 15:33:37 +0900

    feat: 블로그 검색 시 검색어 count 증가 이벤트 추가

--- src/main/java/com/smc2315/blogsearch/service/BlogSearchService.java
+++ src/main/java/com/smc2315/blogsearch/service/BlogSearchService.java
@@ -1,5 +1,7 @@
 package com.smc2315.blogsearch.service;
 
+import com.smc2315.blogsearch.aop.BlogSearchEvent;
+import com.smc2315.blogsearch.aop.PublishEvent;
 import com.smc2315.blogsearch.dto.mapper.BlogSearchMapper;
 import com.smc2315.blogsearch.dto.request.BlogSearchRequest;
 import com.smc2315.blogsearch.dto.response.BlogSearchResponse;
@@ -13,6 +15,7 @@ public class BlogSearchService {
 
     private final BlogSearchApiCaller blogSearchApiCaller;
 
+    @PublishEvent(eventType = BlogSearchEvent.class, params = "#{T(com.smc2315.blogsearch.dto.SendableParameter).of(query)}")
     public BlogSearchResponse getBlogSearchResults(BlogSearchRequest blogSearchRequest) {
         return BlogSearchMapper.INSTANCE
                 .toBlogSearchResponse(blogSearchApiCaller.searchBlogs(blogSearchRequest));
