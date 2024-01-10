93a5fc0c97dba6f55fbb1a2c2c2c9d140caf0557
smc2315 <choeyongtae085@gmail.com>
2024-01-10 18:18:02 +0900

    feat: retryable, recover 추가

--- src/main/java/com/smc2315/blogsearch/config/Configuration.java
+++ src/main/java/com/smc2315/blogsearch/config/Configuration.java
@@ -1,9 +1,11 @@
 package com.smc2315.blogsearch.config;
 
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @org.springframework.context.annotation.Configuration
+@EnableRetry
 @EnableAsync(proxyTargetClass = true)
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class Configuration {
--- src/main/java/com/smc2315/blogsearch/external/BlogSearchApiCaller.java
+++ src/main/java/com/smc2315/blogsearch/external/BlogSearchApiCaller.java
@@ -2,25 +2,36 @@ package com.smc2315.blogsearch.external;
 
 import com.smc2315.blogsearch.dto.request.BlogSearchRequest;
 import com.smc2315.blogsearch.dto.response.ApiResponse;
-import lombok.RequiredArgsConstructor;
+import com.smc2315.blogsearch.external.client.BlogSearchOpenFeign;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
 @Service
 public class BlogSearchApiCaller {
 
-    private final BlogSearchOpenFeign blogSearchOpenFeign;
+    private final BlogSearchOpenFeign kakaoBlogSearchOpenFeign;
+    private final BlogSearchOpenFeign naverBlogSearchOpenFeign;
 
+    public BlogSearchApiCaller(@Qualifier("kakao") BlogSearchOpenFeign kakaoBlogSearchOpenFeign,
+                               @Qualifier("naver") BlogSearchOpenFeign naverBlogSearchOpenFeign) {
+        this.kakaoBlogSearchOpenFeign = kakaoBlogSearchOpenFeign;
+        this.naverBlogSearchOpenFeign = naverBlogSearchOpenFeign;
+    }
+
+    @Retryable(maxAttempts = 1)
     public ApiResponse searchBlogs(BlogSearchRequest blogSearchRequest) {
-        return blogSearchOpenFeign.searchBlogs(
+        return kakaoBlogSearchOpenFeign.searchBlogs(
                 blogSearchRequest.query(),
                 blogSearchRequest.page(),
                 blogSearchRequest.sort()
         );
     }
 
+    @Recover
     public ApiResponse searchBlogsFailureRecover(BlogSearchRequest blogSearchRequest) {
-        return blogSearchOpenFeign.searchBlogs(
+        return naverBlogSearchOpenFeign.searchBlogs(
                 blogSearchRequest.query(),
                 blogSearchRequest.page(),
                 blogSearchRequest.sort()
