b065dc521c70fd77d75647e872887557866f0ea2
smc2315 <choeyongtae085@gmail.com>
2024-01-10 15:32:17 +0900

    feat: PopularSearchService 검색어 count 증가 추가

--- src/main/java/com/smc2315/blogsearch/service/PopularSearchService.java
+++ src/main/java/com/smc2315/blogsearch/service/PopularSearchService.java
@@ -2,6 +2,7 @@ package com.smc2315.blogsearch.service;
 
 import com.smc2315.blogsearch.dto.mapper.PopularSearchMapper;
 import com.smc2315.blogsearch.dto.response.PopularSearchResponse;
+import com.smc2315.blogsearch.entity.PopularSearch;
 import com.smc2315.blogsearch.repository.PopularSearchRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,20 @@ import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
-@Transactional(readOnly = true)
+@Transactional
 public class PopularSearchService {
 
     private final PopularSearchRepository popularSearchRepository;
 
+    public void increaseSearchCount(String keyword) {
+        popularSearchRepository.findByKeyword(keyword)
+                .ifPresentOrElse(
+                        PopularSearch::increaseCount,
+                        () -> popularSearchRepository.save(PopularSearch.create(keyword))
+                );
+    }
+
+    @Transactional(readOnly = true)
     public List<PopularSearchResponse> findTop10Keywords() {
         return popularSearchRepository.findTop10Keywords().stream()
                 .map(PopularSearchMapper.INSTANCE::toPopularSearchResponse)
