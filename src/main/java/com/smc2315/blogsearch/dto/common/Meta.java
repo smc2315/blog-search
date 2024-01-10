ef7aafbd4256ae02dfd0df95c5824e48960945d3
smc2315 <choeyongtae085@gmail.com>
2024-01-10 18:15:43 +0900

    feat: Meta of 추가

--- src/main/java/com/smc2315/blogsearch/dto/common/Meta.java
+++ src/main/java/com/smc2315/blogsearch/dto/common/Meta.java
@@ -2,8 +2,11 @@ package com.smc2315.blogsearch.dto.common;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record Meta(@JsonProperty("is_end")boolean isEnd,
+public record Meta(@JsonProperty("is_end") Boolean isEnd,
                    @JsonProperty("pageable_count") int pageableCount,
                    @JsonProperty("total_count") int totalCount) {
 
+    public static Meta of(Boolean isEnd, int pageableCount, int totalCount) {
+        return new Meta(isEnd, pageableCount, totalCount);
+    }
 }
