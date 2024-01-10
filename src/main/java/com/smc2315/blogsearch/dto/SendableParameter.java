d85b724d527ce1279435ccf9bf80134e252d28f8
smc2315 <choeyongtae085@gmail.com>
2024-01-10 18:13:18 +0900

    refactor: SendableParameter record로 변경

--- src/main/java/com/smc2315/blogsearch/dto/SendableParameter.java
+++ src/main/java/com/smc2315/blogsearch/dto/SendableParameter.java
@@ -1,16 +1,6 @@
 package com.smc2315.blogsearch.dto;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Data
-@RequiredArgsConstructor
-@Getter
-public class SendableParameter {
-
-    private final String keyword;
-
+public record SendableParameter(String keyword) {
     public static SendableParameter of(String keyword) {
         return new SendableParameter(keyword);
     }
